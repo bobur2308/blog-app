a285c5465797498d535dd90366ab5438d786e859
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-11-21 16:58:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ require('dotenv').config()
 
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const connectDb = require('./server/config/db')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')
+const session = require('express-session')
 
 const app = express()
 const port = process.env.PORT || 5000
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(session({
+  secret:"keyboard cat",
+  resave:false,
+  saveUninitialized:true,
+  store:MongoStore.create({
+    mongoUrl:process.env.MONGO_URI
+  })
+}))
 
 // templating engine
 app.use(expressLayouts)
@@ -13,6 +28,9 @@ app.set('layout','./layouts/main')
 app.set('view engine','ejs')
 
 app.use('/',require('./server/routes/main'))
+app.use('/',require('./server/routes/admin'))
+
+connectDb();
 app.listen(port,()=>{
   console.log("App is running on port",port)
 })
